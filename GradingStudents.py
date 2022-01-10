b54b6359b950b72a81c5d49238be181335913fb9
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2022-01-10 22:44:12 +0300

    Update GradingStudents.py

--- GradingStudents.py
+++ GradingStudents.py
@@ -61,3 +61,22 @@ if __name__ == '__main__':
 
   
   --end
+  
+  
+  #alternative
+  for i in range(0,len(grades)):
+        
+        if grades[i] < 38:
+            continue
+        else:
+            temp = grades[i]
+            te = temp % 5
+            if te == 3:
+                temp = temp + 2
+                grades[i] = temp
+            elif te == 4:
+                temp = temp + 1
+                grades[i] = temp
+            else:
+                continue
+    return grades;
