c0b7e5831434a5207c8f38142fa6438372fecf5e
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2022-04-12 18:55:11 +0300

    Create GradingStudents.py

---
+++ Hackerrank Challenges/GradingStudents.py
@@ -0,0 +1,82 @@
+HackerLand University has the following grading policy:
+
+Every student receives a  in the inclusive range from  to .
+Any  less than  is a failing grade.
+Sam is a professor at the university and likes to round each student's  according to these rules:
+
+If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+Examples
+
+ round to  (85 - 84 is less than 3)
+ do not round (result is less than 40)
+ do not round (60 - 57 is 3 or higher)
+Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function gradingStudents in the editor below.
+
+gradingStudents has the following parameter(s):
+
+int grades[n]: the grades before rounding
+Returns
+
+int[n]: the grades after rounding as appropriate
+  
+  
+  --start
+  def gradingStudents(grades):
+    def gradingStudents(grades):
+    for i in range(0,len(grades)):
+        
+        if grades[i] >= 38:
+            temp = grades[i]
+            te = temp % 5
+            if te == 3:
+                temp = temp + 2
+                grades[i] = temp
+            elif te == 4:
+                temp = temp + 1
+                grades[i] = temp
+    return grades;
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
+  
+  --end
+  
+  
+  #alternative
+  for i in range(0,len(grades)):
+        
+        if grades[i] < 38:
+            continue
+        else:
+            temp = grades[i]
+            te = temp % 5
+            if te == 3:
+                temp = temp + 2
+                grades[i] = temp
+            elif te == 4:
+                temp = temp + 1
+                grades[i] = temp
+            else:
+                continue
+    return grades;
