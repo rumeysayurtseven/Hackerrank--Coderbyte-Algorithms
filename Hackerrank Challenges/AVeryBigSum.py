bc352c415385567a89b698517947cf9969f048ab
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2022-04-12 18:50:04 +0300

    Create AVeryBigSum.py

---
+++ Hackerrank Challenges/AVeryBigSum.py
@@ -0,0 +1,45 @@
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+Return
+
+long: the sum of all array elements
+Input Format
+
+The first line of the input consists of an integer .
+The next line contains  space-separated integers contained in the array.
+
+Output Format
+
+Return the integer sum of the elements in the array.
+
+--start
+def aVeryBigSum(ar):
+    sum = 0
+    for i in ar:
+        sum = sum + i
+    return(sum)
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+--end
