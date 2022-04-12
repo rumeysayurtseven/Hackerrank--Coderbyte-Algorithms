3203f7774ff8963004b9071eb7160956475c818d
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2022-04-12 18:45:48 +0300

    Create Easy-CheckNums.py

---
+++ CoderbyteChallenges/Easy-CheckNums.py
@@ -0,0 +1,11 @@
+""" Have the function CheckNums(num1,num2), take both parameters being passed and return the string ture if num2 is greater than num1, 
+otherwise return the string false. If parameters are equal to each other, return the string -1."""
+def CheckNums(num1,num2):
+  if num2 > num1:
+    return "true"
+  elif num2 == num1
+    return -1
+  else:
+    return "false";
+  
+ return num1
