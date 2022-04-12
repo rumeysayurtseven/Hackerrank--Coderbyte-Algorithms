b9b8a0b17583335efda70d58b7b6a7b878c64b69
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2022-04-12 19:13:04 +0300

    Create SetsDifference.py

---
+++ Patika Challenges/SetsDifference.py
@@ -0,0 +1,19 @@
+"""
+Sample Input
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+Sample Output
+4
+"""
+
+"start"
+n = int(input())
+n1 = set(map(int,input().split()))
+b = int(input())
+b1 = set(map(int,input().split()))
+
+setdiff = n1.difference(b1)
+print(len(setdiff));
+"end"
