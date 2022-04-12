dc3b85348847fb9655838f529f804e37eff0b7b6
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2022-04-12 18:51:48 +0300

    Create BirthdayCakeCandles.py

---
+++ Hackerrank Challenges/BirthdayCakeCandles.py
@@ -0,0 +1,42 @@
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+
+The maximum height candles are  units high. There are  of them, so return .
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+
+
+--start
+def birthdayCakeCandles(candles):
+    count=0
+    maxv = max(candles)
+    for i in range(len(candles)):
+        if candles[i] == maxv:
+            count += 1
+    return count;
+                    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
